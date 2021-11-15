bde8c50dd8e1b10a43081afa9a32a218f92680f6
quoccuongpresent <nguyenduyanhquoc1st@gmail.com>
2021-11-15 10:20:43 +0700

    Astar

--- SOURCE/main.py
+++ SOURCE/main.py
@@ -34,6 +34,15 @@ def ucs(graph, initial, goal):
 
     return None, None
 
+def get_manhattan_heuristic(node, goal):    
+    i, j = divmod(int(node), 8)    
+    i_goal, j_goal = divmod(int(goal), 8)    
+    i_delta = abs(i - i_goal)    
+    j_delta = abs(j - j_goal)    
+    
+    manhattan_dist = i_delta + j_delta    
+    return manhattan_dist  
+
 def GBFS(graph, initial, goal):
     path = [0]*len(graph)
     frontier = queue.PriorityQueue()
@@ -65,13 +74,36 @@ def GBFS(graph, initial, goal):
 
     return None, None
 
+def Astar(graph, initial, goal): 
+    path = [0] * len(graph)
+    frontier = queue.PriorityQueue()
+    frontier_list = []
 
+    expanded_state = []
+    frontier.put((0, goal))
+    frontier_list.append(initial)
 
-def get_manhattan_heuristic(node, goal):    
-    i, j = divmod(int(node), 8)    
-    i_goal, j_goal = divmod(int(goal), 8)    
-    i_delta = abs(i - i_goal)    
-    j_delta = abs(j - j_goal)    
-    
-    manhattan_dist = i_delta + j_delta    
-    return manhattan_dist  
+    while len(frontier) > 0:
+        current = frontier.get() 
+        frontier_list.remove(current[1])
+
+        if current[1] == goal: 
+            return getPath(path, initial, goal), expanded_state
+
+        expanded_state.append(current[1])
+
+        for neighbour in graph[current[1]]:
+            path_cost = current[0] - get_manhattan_heuristic(current[1], goal) + 1
+            path_cost_heuristic = path_cost + get_manhattan_heuristic(neighbour, goal)
+
+            if neighbour not in expanded_state and neighbour not in frontier_list: 
+                frontier.put((path_cost_heuristic, neighbour))
+                frontier_list.append(neighbour)
+                path[neighbour] = current[1]
+            elif neighbour in frontier_list: 
+                for node in frontier.queue: 
+                    if node[1] == neighbour and node[0] > path_cost_heuristic: 
+                        frontier.queue[frontier.queue.index(node)] = (path_cost_heuristic, neighbour)
+
+    return None, None
+
